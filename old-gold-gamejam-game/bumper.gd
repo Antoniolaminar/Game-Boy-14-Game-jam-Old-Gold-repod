485668685c96836117e78876055185b85a2df950
AntonioLaminar <dcbantonio19@gmail.com>
2026-09-20 03:29:37 +0100

    Polished respawn - working on bumpers

--- old-gold-gamejam-game/bumper.gd
+++ old-gold-gamejam-game/bumper.gd
@@ -1,14 +1,6 @@
 class_name Bumper
 extends RigidBody2D
 
-@export var hitbox_area_left : CollisionShape2D
-@export var hitbox_area_right : CollisionShape2D
-
-# Makes so the ball receives bounce from bumpers upon hitbox contact
-func resolve_hitboxes() -> void:
-	pass
-	
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,3 +9,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_body_entered_left(body: Node) -> void:
+	if (body as BallObject):
+		var bounce_vector_left = Vector2(700.5,550.0)
+		body.apply_central_impulse(bounce_vector_left)
+		
+		
+	pass # Replace with function body.
+
+
+func _on_body_entered_right(body: Node) -> void:
+	if (body as BallObject):
+		var bounce_vector_right = Vector2(-700.5,550.0)
+		body.apply_central_force(bounce_vector_right)
+	pass # Replace with function body.
